e37ad654fc39d237b5efe0523c4c5f3e9e1084a4
Wilson <wilsonhsieh1216@gmail.com>
2025-10-30 15:29:21 +0000

    feat: add query keys for new endpoints
    - Add POPULAR, TOP_RATED, NOW_PLAYING, TRENDING static keys
    - Add MOVIE_CREDITS, RECOMMENDATIONS, SIMILAR parameterized keys
    - Add PERSON, PERSON_CREDITS for actor integration
    - Maintains consistent pattern with existing keys

--- movies/src/constants/queryKeys.js
+++ movies/src/constants/queryKeys.js
@@ -15,6 +15,26 @@ export const QUERY_KEYS = {
   UPCOMING: ["upcoming"],
 
   /**
+   * Query key for popular movies
+   */
+  POPULAR: ["popular"],
+
+  /**
+   * Query key for top rated movies
+   */
+  TOP_RATED: ["topRated"],
+
+  /**
+   * Query key for now playing movies (in theaters)
+   */
+  NOW_PLAYING: ["nowPlaying"],
+
+  /**
+   * Query key for trending movies
+   */
+  TRENDING: ["trending"],
+
+  /**
    * Query key for a single movie by ID
    * @param {string|number} id - Movie ID
    */
@@ -36,5 +56,34 @@ export const QUERY_KEYS = {
    * @param {string|number} id - Movie ID
    */
   REVIEWS: (id) => ["reviews", { id }],
-};
 
+  /**
+   * Query key for movie credits (cast and crew)
+   * @param {string|number} id - Movie ID
+   */
+  MOVIE_CREDITS: (id) => ["movieCredits", { id }],
+
+  /**
+   * Query key for movie recommendations
+   * @param {string|number} id - Movie ID
+   */
+  RECOMMENDATIONS: (id) => ["recommendations", { id }],
+
+  /**
+   * Query key for similar movies
+   * @param {string|number} id - Movie ID
+   */
+  SIMILAR: (id) => ["similar", { id }],
+
+  /**
+   * Query key for person/actor details
+   * @param {string|number} id - Person ID
+   */
+  PERSON: (id) => ["person", { id }],
+
+  /**
+   * Query key for person's movie credits (filmography)
+   * @param {string|number} id - Person ID
+   */
+  PERSON_CREDITS: (id) => ["personCredits", { id }],
+};
